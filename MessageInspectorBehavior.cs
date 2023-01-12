5775fadff5f922db5e493fd1f893d614eadecddb
Infinite Loop Development Ltd <info@infiniteloop.ie>
2023-01-12 14:28:03 +0100

    Initial commit

---
+++ MessageInspectorBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace ViesCheckSpain
+{
+
+
+    public class MessageInspectorBehavior : IClientMessageInspector, IEndpointBehavior
+    {
+      
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            var buffer = reply.CreateBufferedCopy(int.MaxValue);
+            var receivedMessage = buffer.CreateMessage();
+            Console.WriteLine(receivedMessage);
+            reply = buffer.CreateMessage();
+        }
+
+        public object BeforeSendRequest(ref Message request,
+            System.ServiceModel.IClientChannel channel)
+        {
+            Console.WriteLine(request.ToString());
+            return null;
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        { }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.ClientMessageInspectors.Add(this);
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        { }
+
+        public void Validate(ServiceEndpoint endpoint)
+        { }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceModel;
+
+namespace ViesCheckSpain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vies = new vies.checkVatPortTypeClient(new BasicHttpsBinding(BasicHttpsSecurityMode.Transport), new EndpointAddress("https://ec.europa.eu/taxation_customs/vies/services/checkVatService"));
+            var countryCode = "ES";
+            var vatNumber = "B83891812";
+            var traderName = "Rodrisa Automoviles";
+            var traderCompanyType = "";
+            var traderStreet = "Avda. Reina Victoria";
+            var traderPostcode = "28430";
+            var traderCity = "Madrid";
+            var requesterCountryCode = "";
+            var requesterVatNumber = "";
+            bool valid;
+            string traderAddress;
+            vies.matchCode traderNameMatch;
+            vies.matchCode traderCompanyTypeMatch;
+            vies.matchCode traderStreetMatch;
+            vies.matchCode traderPostcodeMatch;
+            vies.matchCode traderCityMatch;
+            string requestIdentifier;
+            vies.Endpoint.EndpointBehaviors.Add(new MessageInspectorBehavior());
+            vies.checkVatApprox(ref countryCode, ref vatNumber, ref traderName, ref traderCompanyType, ref traderStreet,
+                ref traderPostcode, ref traderCity, requesterCountryCode, requesterVatNumber, out valid,
+                out traderAddress, out traderNameMatch, out traderCompanyTypeMatch, out traderStreetMatch,
+                out traderPostcodeMatch, out traderCityMatch, out requestIdentifier); // Spain
+            //vies.checkVat(ref country, ref vat, out blnValid, out name, out address); // All other EU
+        }
+    }
+}
